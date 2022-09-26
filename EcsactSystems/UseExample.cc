2110729b4114e0b7d129be0730d2f1689467f656
Kelwan <austin@kelway.ca>
2022-09-26 17:58:04 -0400

    Delete old CC file

--- EcsactSystems/UseExample.cc
+++
@@ -1,9 +0,0 @@
-#include "example.ecsact.hh"
-#include "example.ecsact.systems.hh"
-
-void example::UseExample(UseExample::context& ctx) {
-	auto example = ctx.get<Example>();
-
-	example.example_value +=1;
-	ctx.update(example);
-}
